f0fc930404c4d14c863cf497394dda6ff75cc8a5
Ala Dzemyanovich <ala.dzemyanovich@gmail.com>
2019-03-23 14:14:43 +0100

    feat: add solution of money-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     // Your code goes here!
+    let coinValues = [50, 25, 10, 5, 1];
+    let coinNames = ["H", "Q", "D", "N", "P"];
+    //let coinsQuantity = [];
+    let coinResult = {};
+    /*let currency = currency;*/
+    if (currency <= 0) {
+        return {};
+    }
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    for (let i = 0; i < coinValues.length; i++) {
+        if (Math.floor(currency / coinValues[i]) || 0) {
+            coinResult[coinNames[i]] = Math.floor(currency / coinValues[i]);
+            currency -= coinValues[i] * Math.floor(currency / coinValues[i]);
+        }
+    }
     // Return an object containing the minimum number of coins needed to make change
+    console.log(coinResult);
+    return coinResult;
 }
+
